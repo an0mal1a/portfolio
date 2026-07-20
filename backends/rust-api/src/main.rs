0d7892c1b7bae40ef5ecc5555bc4bba471f137ee
an0mal1a <129337574+an0mal1a@users.noreply.github.com>
2026-07-20 14:54:07 +0200

    feat: first implementation of rust-api & config

--- backends/rust-api/src/main.rs
+++ backends/rust-api/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{self, Json, Router, routing::get};
+use serde_json::{Value, json};
+
+async fn health_check() -> Json<Value> {
+    Json(json!({
+        "status": "ok",
+        "message": "Server is running correctly!"
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    let app: Router = Router::new()
+        .route("/", get(health_check));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("Running on: http://localhost:8001");
+    axum::serve(listener, app).await.unwrap();
 }
