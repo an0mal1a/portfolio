1756571906e152986b04d12f226ba77777364872
an0mal1a <129337574+an0mal1a@users.noreply.github.com>
2026-07-21 15:27:04 +0200

    feat: create new database service

---
+++ backends/python-api/services/database/client.py
@@ -0,0 +1,36 @@
+# Internal modules
+from config import POSTGRES_DB, POSTGRES_DB_PORT, POSTGRES_DB_HOST, SYNC_WRITER_USER, SYNC_WRITER_PASSWORD
+from ..errors import MissingData
+ 
+# PostgreSQL
+from psycopg import connect
+
+# Other
+from contextlib import contextmanager
+
+
+class DBClient: 
+    def __init__(self):
+        if not all([POSTGRES_DB, POSTGRES_DB_HOST, SYNC_WRITER_USER, SYNC_WRITER_PASSWORD]):
+            raise MissingData("[SRV.DB.INIT] !> Missing database information on the .env")
+
+        self.username = SYNC_WRITER_USER
+        self.__password = SYNC_WRITER_PASSWORD
+        self.database = POSTGRES_DB
+        self.post = POSTGRES_DB_PORT # default is 5432
+        self.host = POSTGRES_DB_HOST if POSTGRES_DB_HOST else "localhost" 
+
+    """
+    This function returns only 1 connection to postgresql (no pool)
+    """
+    @contextmanager
+    def get_db_connection(self): 
+        with connect(
+            dbname=self.database,
+            user=self.username,
+            password=self.__password,
+            host=self.host,
+            port=self.port,
+        ) as conn:
+            yield conn
+                
---
+++ backends/python-api/services/errors.py
@@ -0,0 +1,5 @@
+# This file has been created for shared errors across services
+
+class MissingData(Exception):
+    """Raised when missing data on a function call"""
+    pass
