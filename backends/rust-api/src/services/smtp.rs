d40412b2a77a3c1c26bea86c7d34c1b34db35d04
an0mal1a <129337574+an0mal1a@users.noreply.github.com>
2026-07-23 22:05:03 +0200

    feat: starting smtp service (untested)

---
+++ backends/rust-api/src/services/smtp.rs
@@ -0,0 +1,97 @@
+// Config
+use crate::config::CONFIG;
+
+use lettre::message::Mailbox;
+use lettre::message::header::ContentType;
+// SMTP Imports
+use lettre::{Message, SmtpTransport, Transport};
+use lettre::transport::smtp::authentication::Credentials;
+
+struct SMTPClient { 
+    pub recipient_email: String,
+    pub smtp_user: String,
+    pub smtp_pass: String,
+    pub smtp_host: String,
+    pub smtp_port: u16,
+}
+
+
+pub enum SMTPError {
+    ConnectionError,
+    MailConstruct,
+    SendError,
+}
+
+impl SMTPClient {
+    pub fn new() -> Self {
+        let cfg = CONFIG.get().expect("[Services.SMTP.new] !> CONFIG not initialized");
+
+        // Check if we have all the needed creds
+        if ![&cfg.smtp_user, &cfg.smtp_pass, &cfg.smtp_host, &cfg.recipient_email].iter().all(|c| !c.is_empty()) {
+            panic!("[Services.SMTP.new] !> SMTP Credentials not found (required=[SMTP_USER,SMTP_PASS,SMTP_HOST,RECIPIENT_EMAIL])")
+        }
+
+        Self {
+            recipient_email: cfg.recipient_email.clone(),
+            smtp_user: cfg.smtp_user.clone(),
+            smtp_pass: cfg.smtp_pass.clone(),
+            smtp_host: cfg.smtp_host.clone(),
+            smtp_port: cfg.smtp_port,
+        }
+    }
+
+    /// This function return a valid SmtpTransport that
+    /// is used to perform the "send" of the preapared
+    /// email.
+    pub fn get_conn_details(&self) -> Result<SmtpTransport, SMTPError> {
+        let creds = Credentials::new(
+            self.smtp_user.clone(),
+            self.smtp_pass.clone()
+        );
+
+        // return the connection made
+        let transport = 
+            match SmtpTransport::relay(&self.smtp_host.to_string()) {
+                Ok(t) => t,
+                Err(e) => {
+                    println!("[Services.SMTP.get_conn_details] !> Error performing connection... smtp_host=({}), err=({})", &self.smtp_host.to_string(), e.to_string());
+                    return Err(SMTPError::ConnectionError)
+                }
+            };
+
+        Ok(transport.credentials(creds).build())
+    }
+
+    /// Function used to send a specific email, the body of 
+    /// the constructed email has to be HTML and be _______
+    pub fn send_mail(&self, subject: String, body: String) -> Result<(), SMTPError> {
+        let email = Message::builder()
+            .from(Mailbox {
+                name: Some("Portfolio".to_string()),
+                email: self.smtp_user.clone().parse().unwrap(),
+            })
+
+            .to(Mailbox {
+                name: Some("Portfolio".to_string()),
+                email: self.recipient_email.clone().parse().unwrap(),
+            })
+
+            .subject(format!("New client inquiry - {}", subject))
+            .header(ContentType::TEXT_HTML)
+            .body(body)
+            .map_err(|_| SMTPError::MailConstruct)?;
+
+        let mailer = self.get_conn_details()?;
+
+        match mailer.send(&email) {
+            Ok(_) => {
+                println!("[Services.SMTP.send_mail] > Mail sended successfully");
+                return Ok(())
+            }
+            Err(e) => {
+                println!("[Services.SMTP.send_mail] !> Could not send email: {e:?}");
+                return Err(SMTPError::SendError)
+            }
+        }
+    }
+}
