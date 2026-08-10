5ae0ae014a301e7ffa65c7f89268e765d7636fb4
an0mal1a <129337574+an0mal1a@users.noreply.github.com>
2026-08-10 16:35:14 +0200

    feat: create jobber client to log in db the jobs results
    *UNTESTED*

--- backends/python-api/crons/tasks/github_sync.py
+++ backends/python-api/crons/tasks/github_sync.py
@@ -3,10 +3,16 @@ from services.github.errors import GitHubInvalidAuth
 from services.github.client import GitHubClient
 from services.github.models import Repository
 
+# Jobber (log) service
+from services.github.jobber import JobberLog
+
 # Shared errors
 from services.errors import MissingData
 
+from datetime import datetime
+
 def sync_gihub(gh_token, gh_user) -> bool: 
+    start_time = datetime.now()
     if not gh_token:
         print("No github token, stopping task")
         return False
@@ -17,25 +23,37 @@ def sync_gihub(gh_token, gh_user) -> bool:
         print(f"[CRONJOB.GH_SYNC_TASK] !> Invalid auth token, details={e}")
         return False
 
-    print("[CRONJOB.GH_SYNC_TASK] > Starting GitHub sync task")
+    db_jobber = JobberLog()
+    db_jobber.job_started()
+    print(f"[CRONJOB.GH_SYNC_TASK] > Starting GitHub sync task [JOB_ID: {db_jobber.job_id}]")
     
     # Get all the repos information
     gh_repos_info: list[Repository] = gh_client.process_repositories()
+
+    # Set repos_found in db
+    db_jobber.update_field("repositories_found", len(gh_repos_info))
     
     for repo in gh_repos_info:
         # Account info
         owner_id = gh_client.add_owner(repo.owner)
         
         if not owner_id:
+            db_jobber.increase_field('repositories_failed')
             print(f"[CRONJOB.GH_SYNC_TASK] !> Missing owner_id, ignoring repo={repo.name}, owner_id={owner_id}")
             continue
 
-        repo_id = gh_client.add_repo(repo, owner_id)
+        repo_id, inserted = gh_client.add_repo(repo, owner_id)
 
         if not repo_id:
+            db_jobber.increase_field('repositories_failed')
             print(f"[CRONJOB.GH_SYNC_TASK] !> Missing repo_id, ignoring repo={repo.name}, repo_id={repo_id}")
             continue
 
+        if inserted:
+            db_jobber.increase_field('repositories_created')
+        else: 
+            db_jobber.increase_field('repositories_updated')
+            
         # contributors         
         for contributors in repo.contributors:
             contributor_id = gh_client.add_owner(contributors)
@@ -48,4 +66,15 @@ def sync_gihub(gh_token, gh_user) -> bool:
         # Repo lang -> Depends on repo info
         for lang in repo.languages:
             gh_client.add_lang(repo_id, lang)
+
+    # Calculate & push elapsed time
+    end_time = datetime.now()
+    elapsed = (end_time - start_time).microseconds
+
+    db_jobber.update_field("completed_at", end_time.isoformat())
+    db_jobber.update_field("duration_ms", elapsed)
+
+    db_jobber.update_field("status", "completed")
+
+
 
---
+++ backends/python-api/services/github/jobber.py
@@ -0,0 +1,83 @@
+from services.database.client import DBClient
+
+from datetime import datetime
+
+class JobberLog:
+    """
+    This service has been created as a "beautiful" way to provide the db
+    connection to the cronjobs tasks, allowing (for github) to set the job started
+    change the status, save details, etc. 
+
+    - Possible `status` field values:
+        - `in_progress`
+        - `completed`
+        - `failed`
+
+    The name jobber is just for pure fun :)
+    """
+    def __init__(self):
+        self.job_id = None
+        self._db_client = DBClient()
+
+        # Define the valid fields used by `update_field` && `increase_field`
+        self.VALID_FIELDS = ["started_at", "completed_at", "status", "repositories_found", "repositories_created", "repositories_updated", "repositories_failed", "duration_ms", "error", ]
+        self.INT_FIELDS = ["repositories_found", "repositories_created", "repositories_updated", "repositories_failed", "duration_ms"]
+
+    def job_started(self) -> bool:
+        """Create de job in db and store the `job_id` in the class context"""
+        with self._db_client.get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    """
+                    INSERT INTO github.sync_jobs (
+                        started_at,
+                        status,
+                    ) VALUES (%s, %s)
+                    RETURNING id
+                    """, (datetime.now().isoformat(),) 
+                )
+
+                gh_id = cur.fetchone()["id"]
+                if gh_id is not None:
+                    self.job_id = gh_id
+                    return True
+
+                return False
+
+    def update_field(self, field, v) -> bool:
+        """
+        Update a given `field` of db to the value of `v`
+        """
+        if field not in self.VALID_FIELDS:
+            raise ValueError("Field not allowed")
+
+        query = f"""
+            UPDATE github_sync_jobs
+            SET {field} = %s
+            WHERE id = %s
+            RETURNING id
+        """
+
+        with self._db_client.get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (v, self.job_id))
+                return cur.rowcount > 0
+
+    def increase_field(self, field, n = 1) -> bool:
+        """
+        Increase a given `field` of db by the amount of `n`.
+        """
+        if field not in self.INT_FIELDS:
+            raise ValueError("Field not allowed")
+
+        query = f"""
+            UPDATE github_sync_jobs
+            SET {field} = {field} + %s
+            WHERE id = %s
+            RETURNING id
+        """
+
+        with self._db_client.get_db_connection() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, (n, self.job_id))
+                return cur.rowcount > 0
